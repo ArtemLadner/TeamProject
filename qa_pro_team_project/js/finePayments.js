a2a0a5190d250d11a42f8108ff17dd10892e4a68
lena <olena.koneva@ezlo.com>
2023-11-25 12:22:06 +0000

    payments

--- qa_pro_team_project/js/finePayments.js
+++ qa_pro_team_project/js/finePayments.js
@@ -1,8 +1,8 @@
 "use strict";
 /**
-Перед вами список полів. Це можна сказати пряме посилання на кожне із полів форми.
-Якщо ви додасте до змінної .value (fineNumber.value) то отримаєте значення
-яке зберігається в цьому полі.
+ Перед вами список полів. Це можна сказати пряме посилання на кожне із полів форми.
+ Якщо ви додасте до змінної .value (fineNumber.value) то отримаєте значення
+ яке зберігається в цьому полі.
  */
 let fineNumber = document.getElementById("fineNumber");
 let passport = document.getElementById("passport");
@@ -16,23 +16,66 @@ let DB = data.finesData;
 
 
 /**
-Вам необхідно реалізувати наступний функціонал.
-Зробити валідацію до всіх полів
-1. Номер та сума повинні бути однакові як в існуючого штрафу - якщо ні видавати
-alert "Номер не співпадає" або "Сума не співпадає"
+ Вам необхідно реалізувати наступний функціонал.
+ Зробити валідацію до всіх полів
+ 1. Номер та сума повинні бути однакові як в існуючого штрафу - якщо ні видавати
+ alert "Номер не співпадає" або "Сума не співпадає"
 
-2. Паспортні дані у форматі - перші дві літери укр алфавіту, та 6 цифр.
-Якщо не співпадає то видавати alert "Не вірний паспортний номер"
+ 2. Паспортні дані у форматі - перші дві літери укр алфавіту, та 6 цифр.
+ Якщо не співпадає то видавати alert "Не вірний паспортний номер"
 
-3. Номер кредитної карки 16 цифр -
-якщо не співпадає то видавати alert "Не вірна кредитна картка"
+ 3. Номер кредитної карки 16 цифр -
+ якщо не співпадає то видавати alert "Не вірна кредитна картка"
 
-4. cvv 3 цифри - якщо не співпадає то видавати alert "Не вірний cvv".
+ 4. cvv 3 цифри - якщо не співпадає то видавати alert "Не вірний cvv".
 
-Якщо валідація проходить успішно, то виконати оплату,
+ Якщо валідація проходить успішно, то виконати оплату,
  тобто вам потрібно видалити обєкт з DB
  */
-buttonSubmit.addEventListener('click',payFine);
-function payFine(){
+buttonSubmit.addEventListener('click', payFine);
 
+function findFineByNumber(value) {
+  return DB.filter((item) => item.номер === value);
+}
+
+function checkPassport(value) {
+  return value.match(/[А-Яа-яІЇЄіїє]{2}\d{6}$/gm);
+}
+
+function checkCreditCardNumber(value) {
+  return value.match(/^\d{16}$/gm);
+}
+
+function checkCvv(value) {
+  return value.match(/^\d{3}$/gm);
+}
+
+function checkFineAmount(value, finesArray) {
+  return finesArray[0]['сума'] === Number(value);
+}
+
+function makePayment(fine) {
+  delete DB[DB.indexOf(fine)];
+}
+
+function payFine() {
+  let fines = findFineByNumber(fineNumber.value);
+  if (fines.length === 0) {
+    alert("Номер не співпадає");
+    return;
+  }
+  if (!checkFineAmount(amount.value, fines)) {
+    alert("Сума не співпадає");
+    return;
+  }
+  if (!checkPassport(passport.value)) {
+    alert("Не вірний паспортний номер");
+  }
+  if (!checkCreditCardNumber(creditCardNumber.value)) {
+    alert("Не вірна кредитна картка");
+  }
+  if (!checkCvv(cvv.value)) {
+    alert("Не вірний cvv");
+  }
+  makePayment(fines[0]);
 }
